cc0bc64fb8b8e0ebeedfe7aa7e9f0b1289853445
Bruce Yang <bruce.yichai20250505@gmail.com>
2026-08-18 11:59:16 +0800

    Publish corrected JPG resolution controls

--- src/utils/filename.js
+++
@@ -1,5 +0,0 @@
-export function unlockedFilename(filename) {
-  const withoutExtension = filename.replace(/\.pdf$/i, '');
-  const withoutSuffix = withoutExtension.replace(/-unlocked$/i, '');
-  return `${withoutSuffix}-unlocked.pdf`;
-}
